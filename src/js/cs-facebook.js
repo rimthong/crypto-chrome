27b283cdab5fafb0331ad441b4b83000b8f5dbaf
louib <louisbvarin@gmail.com>
2013-08-17 12:09:42 -0400

    typo listener & fb

--- src/js/cs-facebook.js
+++ src/js/cs-facebook.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
 
     setText = function (message){
-    
-        // There could be many divs editable
-        // Take the first one
-        target = $("div[g_editable='true']")[0];
-        target.innerText = message;
 
+        target = $("textarea")[0];
+        target.value = message;
+    
     };
 
     getText = function (){
 
-        target = $("div[g_editable='true']")[0];
-        return target.innerText;
+        target = $("textarea")[0];
+        return target.value;
 
     };
 
