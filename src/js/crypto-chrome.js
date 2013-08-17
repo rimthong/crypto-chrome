df8f395facc903a91b9976df04d845400ead11d5
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-08-17 10:31:35 -0400

    Pub/private keys added from armored string

--- src/js/crypto-chrome.js
+++ src/js/crypto-chrome.js
@@ -20,7 +20,12 @@ function crypto-chrome() {
   };
   this.list_public_keys = list_public_keys;
 
-  function add_public_key(master_password, key, callback) {
+  function add_public_key_from_armored(master_password, armored_key, callback) {
+    key = this.openpgp.read_publicKey(armored_key);
+    if(!key) {
+      return callback("Wrong key format.");
+    }
+
     var keys = sjcl.decrypt(window.localStorage['crypto-chrome-pub']);
     if(keys) {
       keys.append(key);
@@ -29,7 +34,7 @@ function crypto-chrome() {
     }
     return callback("Wrong master key");
   }
-  this.add_public_key = add_public_key;
+  this.add_public_key_from_armored = add_public_key_from_armored;
 
   function list_private_keys(master_password, callback) {
     var keys = sjcl.decrypt(master_password, window.localStorage['crypto-chrome-private']);
@@ -40,7 +45,12 @@ function crypto-chrome() {
   };
   this.list_private_keys = list_private_keys;
 
-  function add_private_key(master_password, key, callback) {
+  function add_private_key_from_armored(master_password, key, callback) {
+    key = this.openpgp.read_privateKey(armored_key);
+    if(!key) {
+      return callback("Wrong key format.");
+    }
+
     var keys = sjcl.decrypt(window.localStorage['crypto-chrome-private']);
     if(keys) {
       keys.append(key);
@@ -49,7 +59,7 @@ function crypto-chrome() {
     }
     return callback("Wrong master key");
   }
-  this.add_private_key = add_private_key;
+  this.add_private_key_from_armored = add_private_key_from_armored;
 
 
   function find_key_by_email(master_password, email, callback) {
