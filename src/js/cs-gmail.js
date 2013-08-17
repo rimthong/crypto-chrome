59218f795bf8468244318b47a8a3ae37aa3e0704
louib <louisbvarin@gmail.com>
2013-08-17 11:12:43 -0400

    setText getText gmail inbox

--- src/js/cs-gmail.js
+++ src/js/cs-gmail.js
@@ -1,9 +1,22 @@
-inject_text = function (message){
+$(document).ready(function(){
 
-    // There could be many divs editable
-    // Take the first one
-    target = $("div[g_editable='true']")[0];
-    target.innerText = message;
+    setText = function (message){
+    
+        // There could be many divs editable
+        // Take the first one
+        target = $("div[g_editable='true']")[0];
+        target.innerText = message;
 
-};
+    };
+
+    getText = function (){
+
+        target = $("div[g_editable='true']")[0];
+        return target.innerText;
+
+    };
+
+
+
+});
 
