3323f1bb8bb2032bd0dc165b86027e991c5ae931
louib <louisbvarin@gmail.com>
2013-08-17 13:31:14 -0400

    outlook + fb

--- src/js/cs-outlook.js
+++ src/js/cs-outlook.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
 
     setText = function (message){
-    
-        // There could be many divs editable
-        // Take the first one
-        target = $("div[g_editable='true']")[0];
-        target.innerText = message;
 
     };
 
     getText = function (){
 
-        target = $("div[g_editable='true']")[0];
-        return target.innerText;
+        return "nope";
 
     };
 
@@ -28,4 +22,5 @@ $(document).ready(function(){
 
 
 });
+
 
