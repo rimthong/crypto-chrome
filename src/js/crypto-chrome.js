c61ecbea1db468c4fcdb6a1e76fa7a0c896c95d7
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-08-17 10:23:01 -0400

    Added add and list keys

--- src/js/crypto-chrome.js
+++ src/js/crypto-chrome.js
@@ -1,24 +1,65 @@
-var crypto-chrome = {
+function crypto-chrome() {
+  this.openpgp = openpgp.init();
 
-  'encrypt': function(message, key, callback) {
+  function encrypt(message, key, callback) {
 
-  },
-  'decrypt': function(message, key, callback) {
+  };
+  this.encrypt = encrypt;
 
-  },
-  'sign': function(message, key, callback) {
-    return callback('patate');
-  },
-  'verify': function(message, key, callbac) {
+  function decrypt(message, key, callback) {
 
-  },
-  'list_keys': function(callback) {
+  };
+  this.decrypt = decrypt;
 
-  },
-  'add_key': function(key, callback) {
+  function list_public_keys(master_password, callback) {
+    var keys = sjcl.decrypt(master_password, window.localStorage['crypto-chrome-pub']);
+    if(keys) {
+      return callback(null, keys);
+    }
+    return callback("Wrong master key");
+  };
+  this.list_public_keys = list_public_keys;
 
-  },
-  'delete_key': function(key, callback) {
-
+  function add_public_key(master_password, key, callback) {
+    var keys = sjcl.decrypt(window.localStorage['crypto-chrome-pub']);
+    if(keys) {
+      keys.append(key);
+      window.localStorage['crypto-chrome-pub'] = sjcl.encrypt(master_password, keys);
+      return callback();
+    }
+    return callback("Wrong master key");
   }
+  this.add_public_key = add_public_key;
+
+  function list_private_keys(master_password, callback) {
+    var keys = sjcl.decrypt(master_password, window.localStorage['crypto-chrome-private']);
+    if(keys) {
+      return callback(null, keys);
+    }
+    return callback("Wrong master key");
+  };
+  this.list_private_keys = list_private_keys;
+
+  function add_private_key(master_password, key, callback) {
+    var keys = sjcl.decrypt(window.localStorage['crypto-chrome-private']);
+    if(keys) {
+      keys.append(key);
+      window.localStorage['crypto-chrome-private'] = sjcl.encrypt(master_password, keys);
+      return callback();
+    }
+    return callback("Wrong master key");
+  }
+  this.add_private_key = add_private_key;
+
+
+  function find_key_by_email(master_password, callback) {
+    this.list_public_keys(master_password, function(err, keys) {
+      if(err) {
+        return callback(err);
+      }
+
+      var keys 
+
+      return callback(null, keys);
 };
+
